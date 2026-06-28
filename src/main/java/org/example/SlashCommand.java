57a4f8fa3c80f93db6a659c7325d754c199e0c65
AndriiHubenok <andrewhu08@gmail.com>
2026-06-28 16:00:41 +0300

    add best_cat command

--- src/main/java/org/example/SlashCommand.java
+++ src/main/java/org/example/SlashCommand.java
@@ -1,119 +1,46 @@
 package org.example;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import io.github.cdimascio.dotenv.Dotenv;
-import org.example.api.ParameterStringBuilder;
+import net.dv8tion.jda.api.EmbedBuilder;
+import org.example.api.ApiInteraction;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URL;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
+import java.awt.*;
 
 public class SlashCommand {
-    private static final String BANK_URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange";
+    private ApiInteraction apiInteraction;
 
-    public String getCurrency(String currency){
-        try {
-            LocalDate date = LocalDate.now();
-            String urlWithParams = BANK_URL + "?json=true&valcode=" + currency
-                    + "&date=" + date.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-            URI uri = new URI(urlWithParams);
-            URL url = uri.toURL();
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
-            con.setRequestMethod("GET");
-
-            con.setRequestProperty("Accept", "application/json");
-
-            int status = con.getResponseCode();
-
-            if (status == 200) {
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuilder content = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                in.close();
-                con.disconnect();
-
-                String jsonResponse = content.toString();
-                JsonArray jsonArray = JsonParser.parseString(jsonResponse).getAsJsonArray();
-
-                if (!jsonArray.isEmpty()) {
-                    JsonObject currencyObj = jsonArray.get(0).getAsJsonObject();
-
-                    String currencyName = currencyObj.get("txt").getAsString();  // "Євро"
-                    double rate = currencyObj.get("rate").getAsDouble();         // 51.131
-                    String code = currencyObj.get("cc").getAsString();           // "EUR"
-
-                    switch (currency) {
-                        case "USD" -> currencyName = ":flag_us: :hamburger:";
-                        case "EUR" -> currencyName = ":flag_eu: :rainbow_flag:";
-                    }
-
-                    return String.format("1 %s %s = %.2f UAH", code, currencyName, rate);
-                } else {
-                    return "Error: The bank returned an empty list.";
-                }
-            } else {
-                System.err.println("Failed to fetch. HTTP Status: " + status);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "Error";
+    public SlashCommand() {
+        apiInteraction = new ApiInteraction();
     }
 
-    public String getCatImage() {
-        Dotenv dotenv = Dotenv.load();
-        try {
-            URL url = new URL("https://api.thecatapi.com/v1/images/search");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("Accept", "application/json");
-            //con.setRequestProperty("x-api-key", dotenv.get("CAT_API_KEY"));
 
-            int status = con.getResponseCode();
+    public EmbedBuilder getCurrencies(String[] currencies) {
+        StringBuilder responseBuilder = new StringBuilder();
 
-            if (status == 200) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuilder content = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                in.close();
-                con.disconnect();
-
-                String jsonResponse = content.toString();
-                JsonArray jsonArray = JsonParser.parseString(jsonResponse).getAsJsonArray();
-
-                if (!jsonArray.isEmpty()) {
-                    JsonObject catObj = jsonArray.get(0).getAsJsonObject();
-                    return catObj.get("url").getAsString();
-                } else {
-                    return "Error: The cat API returned an empty list.";
-                }
-            } else {
-                System.err.println("Failed to fetch. HTTP Status: " + status);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        for (String currency : currencies) {
+            String currencyInfo = apiInteraction.getCurrency(currency);
+            responseBuilder.append(currencyInfo).append("\n");
         }
-        return "Error fetching cat image.";
+
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setTitle("бігом в абмєнік");
+        embed.setColor(Color.YELLOW);
+        embed.setDescription(responseBuilder.toString().trim());
+        return embed;
+    }
+
+    public EmbedBuilder getCat(String breed) {
+        String catImageUrl = apiInteraction.getCatImage(breed);
+        if (catImageUrl == null) {
+            return null;
+        }
+
+        EmbedBuilder embed = new EmbedBuilder();
+        switch (breed) {
+            case "siam" -> embed.setTitle("чєрнаморд");
+            default -> embed.setTitle("утіпуті");
+        }
+        embed.setColor(Color.ORANGE);
+        embed.setImage(catImageUrl);
+        return embed;
     }
 }
--- src/main/java/org/example/SlashCommandListener.java
+++ src/main/java/org/example/SlashCommandListener.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
+import java.awt.*;
 
 public class SlashCommandListener extends ListenerAdapter {
     private SlashCommand slashCommand;
@@ -22,6 +25,7 @@ public class SlashCommandListener extends ListenerAdapter {
                         .addOption(OptionType.STRING, "content", "What the bot should say", true),
                 Commands.slash("currency", "Get the current exchange rate for USD"),
                 Commands.slash("cat", "Get a beautiful image of a cat"),
+                Commands.slash("best_cat", "Get an image of best breed of cats"),
                 Commands.slash("leave", "Makes the bot leave the server")
         ).queue();
     }
@@ -35,17 +39,31 @@ public class SlashCommandListener extends ListenerAdapter {
             }
             case "currency" -> {
                 event.deferReply().queue();
-                String resultUsd = slashCommand.getCurrency("USD");
-                String resultEur = slashCommand.getCurrency("EUR");
-                event.getHook().sendMessage("бігом в абмєннік\n" + resultUsd + "\n" + resultEur).queue();
+                String[] currencies = {"USD", "EUR"};
+                EmbedBuilder result = slashCommand.getCurrencies(currencies);
+                event.getHook().sendMessageEmbeds(result.build()).queue();
             }
             case "cat" -> {
                 event.deferReply().queue();
-                String catImageUrl = slashCommand.getCatImage();
-                event.getHook().sendMessage(catImageUrl).queue();
+                EmbedBuilder embed = slashCommand.getCat("");
+                if (embed == null) {
+                    event.getHook().sendMessage("а гдє").queue();
+                }
+
+                event.getHook().sendMessageEmbeds(embed.build()).queue();
+            }
+            case "best_cat" -> {
+                event.deferReply().queue();
+                EmbedBuilder embed = slashCommand.getCat("siam");
+                if (embed == null) {
+                    event.getHook().sendMessage("майкл нє в настроєніі").queue();
+                    return;
+                }
+
+                event.getHook().sendMessageEmbeds(embed.build()).queue();
             }
             case "leave" -> {
-                event.reply("I'm leaving the server now!")
+                event.reply("гудбай")
                         .setEphemeral(true)
                         .flatMap(m -> event.getGuild().leave())
                         .queue();
